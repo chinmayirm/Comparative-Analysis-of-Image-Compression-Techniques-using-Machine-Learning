8290496083a75b4e976954b111fc023dae650130
chinmayirm <121074867+chinmayirm@users.noreply.github.com>
2025-01-23 11:43:15 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O 
+import pandas as pd 
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
